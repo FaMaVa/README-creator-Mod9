bbcda42d791909680a5bf6b9521f7a73a5452eb2
Faith Vaz <faith.m.vaz@gmail.com>
2022-10-25 20:03:36 -0600

    add license section, add content to generate README file, add questions, installed inquirer package

--- index.js
+++ index.js
@@ -1,13 +1,82 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMD = require('./utils/generateMarkdown');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is your GitHub username?",
+    "What is your email address?",
+    "What is your project's name?",
+    "Please write a short description or your project:",
+    "What kind of license should your project have?", //options displayed: MIT, APACHE 2.0, GPL 3.0, BSD 3, None)
+    "What command should be run to install dependencies?", //default: npm i
+    "What command should be run to run tests?", //default: npm test
+    "What does the user need to know about using the repo?",
+    "What does the user need to know about contributing to the repo?",
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    // fs.writeFile(fileName, JSON.stringify(data), (err) =>
+    //     err ? console.log(err) : console.log('Generating R')
+    // ); 
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'github',
+                message: questions[0],
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: questions[1],
+            },
+            {
+                type: 'input',
+                name: 'title',
+                message: questions[2],
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: questions[3],
+            },
+            {
+                type: 'list',
+                name: 'license',
+                message: questions[4],
+                choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+            },
+            {
+                type: 'input',
+                name: 'installation',
+                default: "npm i",
+                message: questions[5],
+            },
+            {
+                type: 'input',
+                name: 'tests',
+                default: "npm test",
+                message: questions[6],
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: questions[7],
+            },
+            {
+                type: 'input',
+                name: 'contributing',
+                message: questions[8],
+            },
+        ])
+};
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,119 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+let badge;
+
+function renderLicenseBadge(license) {
+  if (license === "MIT") {
+    badge = `(https://img.shields.io/badge/License-MIT-yellow.svg)`;
+    return badge;
+  } else if (license === "APACHE 2.0") {
+    badge = `(https://img.shields.io/badge/License-Apache_2.0-blue.svg)`;
+    return badge;
+  } else if (license === "GPL 3.0") {
+    badge = `(https://img.shields.io/badge/License-GPLv3-blue.svg)`;
+    return badge;
+  } else if (license === "BSD 3") {
+    badge = `(https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)`;
+    return badge;
+  } else {
+    badge = ``;
+    return badge;
+  };
+};
+
+renderLicenseBadge(data.license);
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+let link;
+
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    link = `(https://opensource.org/licenses/MIT)`;
+    return link;
+  } else if (license === "APACHE 2.0") {
+    link = `(https://opensource.org/licenses/Apache-2.0)`;
+    return link;
+  } else if (license === "GPL 3.0") {
+    link = `(https://www.gnu.org/licenses/gpl-3.0)`;
+    return link;
+  } else if (license === "BSD 3") {
+    link = `(https://opensource.org/licenses/BSD-3-Clause)`;
+    return link;
+  } else {
+    link = ``;
+    return link;
+  }; 
+};
+
+renderLicenseLink(data.license);
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+let licenseSection;
+
+function renderLicenseSection(license) {
+  if (license === "MIT" || license === "APACHE 2.0" || license === "GPL 3.0" || license === "BSD 3") {
+    licenseSection = `![GitHub License]${badge}${link}`
+    return licenseSection;
+  } else {
+    licenseSection = ``;
+    return licenseSection;
+  };
+};
+
+renderLicenseSection(data.license);
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ${licenseSection}
+  
+  ## Description
 
+  ${data.description}
+
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+
+  To Install necessary dependencies, run the following command:
+
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  This project is licensed under the ${data.license} license.
+
+  ##Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  To run tests, run the following command:
+
+  \`\`\`
+  ${data.tests}
+  \`\`\`
+
+  ## Questions
+
+  If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at ${data.github}.
 `;
 }
 
